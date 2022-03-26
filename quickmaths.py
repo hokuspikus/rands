e8a63f52113bf615961894b6b505a4f7a1fc35b0
Iwo Pikus <hokuspikus90@gmail.com>
2022-03-26 12:17:32 +0100

    first exercise

---
+++ quickmaths.py
@@ -0,0 +1,14 @@
+def quickmaths():
+    x = int(input("Please provide your number: "))
+    y = int(input("Please provide another number: "))
+    print(f"""
+        {x} + {y} = {x + y}
+        {x} - {y} = {x - y}
+        {x} * {y} = {x * y}
+        {x} / {y} = {x / y}
+        {x} // {y} = {x // y}
+        {x} % {y} = {x % y}
+        {x} ** {y} = {x ** y}
+        """)
+
+quickmaths()
